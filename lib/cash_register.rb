21af050e89a2625ba2925bddc912f7422ef00ad6
Keesha Hargrove <kharg14@gmail.com>
2020-07-31 02:34:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -34,10 +34,7 @@ class CashRegister
     end
   end
 
- def void_last_transaction()
-    @total -= @last_transaction_amount
-  end
-
+ 
 
   def void_last_transaction
     self.total = self.total - self.last_transaction
